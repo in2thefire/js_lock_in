7be855d225def10e6fda5c9051a91e284ba200f4
Bohdan Kyrylko <in2thefire.code@gmail.com>
2025-09-10 15:45:02 +0200

    Initial commit

---
+++ script_1_1.js
@@ -0,0 +1,16 @@
+// 1
+const apples = 47;
+const grapes = 135;
+const total = apples + grapes;
+const diff = grapes - apples;
+console.log(total);
+console.log(diff);
+
+//2
+let students = 100;
+students = students + 50;
+students += 50;
+console.log(students);
+
+//
+
