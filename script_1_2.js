59d35fb7d727dc76017ed098504cf5dba15a3097
Bohdan Kyrylko <in2thefire.code@gmail.com>
2025-09-11 20:12:12 +0200

    if/else

---
+++ script_1_2.js
@@ -0,0 +1,18 @@
+//1
+const value = 5
+const value1 = 7
+if(value > value1){
+    console.log('det er rett');
+}else if(value === value1){
+    console.log('det same');
+}else{
+    console.log('ikkje bra');
+}
+
+//2
+const a = 0;
+if(a){
+    console.log('true');
+}else{
+    console.log('false');
+}
