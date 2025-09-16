930e310258f2d2ec10dbbb6d69e560629960c529
Bohdan Kyrylko <in2thefire.code@gmail.com>
2025-09-16 12:38:56 +0200

    Update script_2_1.js

--- module_2/script_2_1.js
+++ module_2/script_2_1.js
@@ -1,10 +1,20 @@
 //1
 const array = [1, 'Hello', null, true, [1,2,3], 10, 'Hi'];
 console.log(array);
-console.log(Array.isArray(array));
+// console.log(Array.isArray(array));
 
-const first = array[0];
-const second = array[1];
-const last = array[array.length - 1];
-console.log(first, second, last);
+// const first = array[0];
+// const second = array[1];
+// const last = array[array.length - 1];
+// console.log(first, second, last);
+// console.log(array.length);
 
+for(let i = 0; i < array.length; i += 1){
+    if(Array.isArray(array[i])){
+        for(let j = 0; j < array[i].length; j += 1){
+            console.log(array[i][j]);
+        }
+        continue
+    }
+    console.log(array[i]);
+}
