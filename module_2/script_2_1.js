184731b5c2434a3eff2abd11b00c85c1d74ff58f
Bohdan Kyrylko <in2thefire.code@gmail.com>
2025-09-16 13:31:31 +0200

    різні типи данних

--- module_2/script_2_1.js
+++ module_2/script_2_1.js
@@ -1,6 +1,6 @@
 //1
-const array = [1, 'Hello', null, true, [1,2,3], 10, 'Hi'];
-console.log(array);
+// const array = [1, 'Hello', null, true, [1,2,3], 10, 'Hi'];
+// console.log(array);
 // console.log(Array.isArray(array));
 
 // const first = array[0];
@@ -26,10 +26,35 @@ console.log(array);
 // }
 
 //4
-for(let i = 0; i < array.length; i+=1){
-    if(typeof array[i] === 'string'){
-        array[i] = 'Змінили'
-    }
-    console.log(array[i]);
-}
+// for(let i = 0; i < array.length; i+=1){
+//     if(typeof array[i] === 'string'){
+//         array[i] = 'Змінили'
+//     }
+//     console.log(array[i]);
+// }
+// console.log(array);
+
+//5
+// let i = 0;
+// while(i < array.length){
+//     console.log(array[i]);
+//     i += 1;
+// }
+
+//6 
+// let value = 1;
+// let test = value;
+// value = 'hi';
+
+// console.log(value);
+// console.log(test);
+
+//7
+const array = [1,2,3,4,5];
+const copy = array;
+
+array.push(6,7);
+copy.push(8,9);
+
 console.log(array);
+console.log(copy);
