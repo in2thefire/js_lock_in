18618964047aff9fff07027258c5a5d7dcde1f59
Bohdan Kyrylko <in2thefire.code@gmail.com>
2025-09-18 14:31:44 +0200

    Update script_2_2.js

--- module_2/script_2_2.js
+++ module_2/script_2_2.js
@@ -1,12 +1,38 @@
-//1
-const arr = [1,2,3,4,5];
+//1 func declaration
+// const arr = [1,2,3,4,5];
 
-function sum(arr){
-for(i = 0; i < arr.length; i += 1){
-    arr[i] = arr[i] * 2;
-}
-console.log(arr);
-}
+// function sum(arr){
+// for(i = 0; i < arr.length; i += 1){
+//     arr[i] = arr[i] * 2;
+// }
+// console.log(arr);
+// }
 
-sum(arr);
-sum([1,5,6,89,2]);
+// sum(arr);
+// sum([1,5,6,89,2]);
+
+//2 func expression
+// const ass = function(arr){
+//     for(i = 0; i < arr.length; i += 1){
+//         arr[i] = arr[i] * 2;
+// }
+//     console.log(arr);
+// }
+
+// ass([1,2,3,4,5]);
+
+//3
+function sum(){
+    // const arr = Array.from(arguments);
+    const arr = [...arguments];
+    let add = 0;
+    console.log(arr);
+    // for(let i = 0;i < arguments.length;i +=1){
+    //     add = add + arguments[i];
+    // }
+    for(const arg of arr){
+        add += arg;
+    }
+    console.log(add);
+}
+sum(1,2,3,45,6,78,9,0);
