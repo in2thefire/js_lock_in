8b1c1b8e4560b7983896c02cb587681bd96a6cfe
Bohdan Kyrylko <in2thefire.code@gmail.com>
2025-09-21 09:47:51 +0200

    3_1_object

---
+++ module_3/script_3_1.js
@@ -0,0 +1,28 @@
+//1 
+const user = {
+    name:'Bohdan',
+    age: 24,
+    languages: {
+        html: false,
+        css: false,
+        js: true,
+        React_Native: false
+    }
+}
+// console.log(user.languages.js);
+// console.log(user['languages']['js']);
+
+// for(const key in user){
+//     console.log(user[key]);
+// }
+
+// user.age = 25;
+// user.age += 1;
+
+//Object.freeze(user); заморозка змін в обєкті
+
+user.city = 'Ølen';
+console.log(user);
+
+delete user.city;
+console.log(user);
