b462992bddb97155e44e53b7fd7db15341fb004e
Bohdan Kyrylko <in2thefire.code@gmail.com>
2025-09-24 22:47:13 +0200

    Update script_3_2.js

--- module_3/script_3_2.js
+++ module_3/script_3_2.js
@@ -1,10 +1,9 @@
 //1
-const arr = [1,2,3];
-const [ _, first] = arr;
-console.log(first);
+// const arr = [1,2,3];
+// const [ _, first] = arr;
+// console.log(first);
 
-
-
+//2
 const user = {
     name: "Bohdan",
     age: 24,
@@ -14,3 +13,10 @@ const user = {
         react: false
     }
 }
+
+const {name, age, skills:{html, js, react}} = user;
+console.log(name);
+console.log(age);
+console.log(html);
+console.log(js);
+console.log(react);
