d37d9862d219de8096aa8f59ce825c42840d3d0f
Bohdan Kyrylko <in2thefire.code@gmail.com>
2025-09-27 15:02:16 +0200

    //13

--- module_3/script_3_2.js
+++ module_3/script_3_2.js
@@ -221,3 +221,26 @@
 //     hexColors.push(color.hex);
 //     rgbColors.push(color.rgb);
 //   }
+
+//13
+const products = [
+  { name: "Radar", price: 1300, quantity: 4 },
+  { name: "Scanner", price: 2700, quantity: 3 },
+  { name: "Droid", price: 400, quantity: 7 },
+  { name: "Grip", price: 1200, quantity: 9 },
+];
+
+function getProductPrice(productName) {
+  // Change code below this line
+  for(const key of products){
+      if(key.name === productName){
+          return key.price
+      }
+  }
+        return null
+  }
+
+
+  // Change code above this line
+
+
