2c2873fa73838fe75a10ca1054b86c850e2bd8e0
Bohdan Kyrylko <in2thefire.code@gmail.com>
2025-09-27 14:36:40 +0200

    //11

--- module_3/script_3_2.js
+++ module_3/script_3_2.js
@@ -191,3 +191,17 @@
 //     email : 'in2thefiree@gmail.com',
 //     list: 'friends',
 // }));
+
+//11
+// function countTotalSalary(salaries) {
+//   let totalSalary = 0;
+//   // Change code below this line
+//   const values = Object.values(salaries);
+//   for(const value of values){
+//     totalSalary += value;
+//   }
+//   // Change code above this line
+//   return totalSalary;
+// }
+
+// console.log(countTotalSalary({ mango: 100, poly: 150, alfred: 80 }));
