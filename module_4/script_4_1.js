7e281c3f6b750ce49dee501eb016629248b0fe9a
Bohdan Kyrylko <in2thefire.code@gmail.com>
2025-09-29 19:26:02 +0200

    Update script_4_1.js

--- module_4/script_4_1.js
+++ module_4/script_4_1.js
@@ -1,15 +1,15 @@
-// Колбек-функція
-function greet(name) {
-  console.log(`Ласкаво просимо ${name}.`);
-}
+// // Колбек-функція
+// function greet(name) {
+//   console.log(`Ласкаво просимо ${name}.`);
+// }
 
-// Функція вищого порядку
-function registerGuest(name, callback) {
-  console.log(`Реєструємо гостя ${name}.`);
-  callback(name);
-}
+// // Функція вищого порядку
+// function registerGuest(name, callback) {
+//   console.log(`Реєструємо гостя ${name}.`);
+//   callback(name);
+// }
 
-registerGuest("Манго", greet);
+// registerGuest("Манго", greet);
 
 
 
@@ -17,3 +17,24 @@ registerGuest("Манго", greet);
 // arr.forEach(function(element, index){
 //     console.log(`${index + 1} : ${element}`);
 // })
+
+const arr = [2,4,2,5,8,9];
+function each(arr, callback){
+  let total = 1;
+  for(const item of arr){
+    total = callback(total, item)
+  }
+  return total
+}
+const sum = (first, second) => first + second;
+const multi = (first, second) => first * second;
+
+console.log(each(arr, sum));
+
+
+
+// const hi = () => {
+//   console.log('Hi');
+// }
+
+// hi()
