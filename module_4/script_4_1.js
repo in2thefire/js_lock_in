062b9ca30ca46f62bb400a8652b7a773196a39e0
Bohdan Kyrylko <in2thefire.code@gmail.com>
2025-09-30 16:20:06 +0200

    Update script_4_1.js

--- module_4/script_4_1.js
+++ module_4/script_4_1.js
@@ -24,10 +24,37 @@
 
 // hi()
 
-const arr = [1,3,4,7,9,2];
-function add(arr){
-    total = arr[0];
-    for(i = 1; i < arr.length; i += 1){
-        total += arr[i];
+// const arr = [1,3,4,7,9,2];
+// function add(arr){
+//     total = arr[0];
+//     for(i = 1; i < arr.length; i += 1){
+//         total += arr[i];
+//     }
+// }
+
+
+// const arr = [1,3,7,9,2,4,5,6];
+// arr.forEach(function(element, index, arr){
+//     console.log(`${index + 1} : ${element}`);
+//     arr[index] = element * 2;
+// })
+
+// console.log(arr);
+
+const arr = [1,3,7,9,2,4,5,6];
+
+function each(arr , callback){
+    let total = arr[0];
+    for(let i = 0; i < arr.length; i += 1){
+        total = callback(total, arr[i]);
     }
+    return total
 }
+
+const mult = (a,b) => a * b;
+const devision = (a,b) => a / b;
+const add = (a,b) => a + b;
+
+console.log(each(arr,mult));
+console.log(each(arr,add));
+console.log(each(arr,devision));
