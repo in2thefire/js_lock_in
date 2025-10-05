8a244c56b322bc940299ddec762dd2a247ae275e
Bohdan Kyrylko <in2thefire.code@gmail.com>
2025-10-05 20:17:47 +0200

    Update script_4_2.js

--- module_4/script_4_2.js
+++ module_4/script_4_2.js
@@ -1,3 +1,9 @@
-const arr = [1, 2, 3, 4, 5, 6, 7];
-const mapArr = arr.map((item) => item * 2);
-console.log(mapArr);
+// const arr = [1, 2, 3, 4, 5, 6, 7];
+// const mapArr = arr.map((item) => item * 2);
+// console.log(mapArr);
+
+// const result = arr.map(item => !(item % 2) ? item * 2 : item);
+// console.log(result);
+
+// const findArr = arr.find(item => item > 5)
+// console.log(findArr);
