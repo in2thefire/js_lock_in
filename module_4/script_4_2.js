1b9e4ecdc2790253f82939cd2671809e8b7d0a2d
Bohdan Kyrylko <in2thefire.code@gmail.com>
2025-10-09 13:15:11 +0200

    Update script_4_2.js

--- module_4/script_4_2.js
+++ module_4/script_4_2.js
@@ -229,35 +229,47 @@ const cars = [
 // const topRatedBooks = books.filter((book) => book.rating >= MIN_RATING);
 // const booksByAuthor = books.filter((book) => book.author === AUTHOR);
 
-const books = [
-  {
-    title: "The Last Kingdom",
-    author: "Bernard Cornwell",
-    rating: 8.38,
-  },
-  {
-    title: "Beside Still Waters",
-    author: "Robert Sheckley",
-    rating: 8.51,
-  },
-  {
-    title: "The Dream of a Ridiculous Man",
-    author: "Fyodor Dostoevsky",
-    rating: 7.75,
-  },
-  { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
-  { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
-];
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     rating: 8.38,
+//   },
+//   {
+//     title: "Beside Still Waters",
+//     author: "Robert Sheckley",
+//     rating: 8.51,
+//   },
+//   {
+//     title: "The Dream of a Ridiculous Man",
+//     author: "Fyodor Dostoevsky",
+//     rating: 7.75,
+//   },
+//   { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+//   { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
+// ];
+// // Change code below this line
+
+// const sortedByAuthorName = [...books].sort((a, b) =>
+//   a.author.localeCompare(b.author)
+// );
+
+// const sortedByReversedAuthorName = [...books].sort((a, b) =>
+//   b.author.localeCompare(a.author)
+// );
+
+// const sortedByAscendingRating = [...books].sort((a, b) => a.rating - b.rating);
+
+// const sortedByDescentingRating = [...books].sort((a, b) => b.rating - a.rating);
+
+// // Change code below this line
+// const sortByAscendingBalance = users => {
+//    return users.sort((a, b) => a.balance - b.balance)
+// };
+// // Change code above this line
+
 // Change code below this line
-
-const sortedByAuthorName = [...books].sort((a, b) =>
-  a.author.localeCompare(b.author)
-);
-
-const sortedByReversedAuthorName = [...books].sort((a, b) =>
-  b.author.localeCompare(a.author)
-);
-
-const sortedByAscendingRating = [...books].sort((a, b) => a.rating - b.rating);
-
-const sortedByDescentingRating = [...books].sort((a, b) => b.rating - a.rating);
+const sortByDescendingFriendCount = (users) => {
+  return [...users].sort((a, b) => b.friends.length - a.friends.length);
+};
+// Change code above this line
