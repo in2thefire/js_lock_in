44b93cd8e01ee3b27a09cc9fd8a1734a6343c139
Bohdan Kyrylko <in2thefire.code@gmail.com>
2025-10-11 14:12:45 +0200

    Update script_5_1.js

--- module_5/script_5_1.js
+++ module_5/script_5_1.js
@@ -1,17 +1,30 @@
-"use strict";
+// "use strict";
 
-function foo() {
-  console.log(this);
-}
-foo();
+// function foo() {
+//   console.log(this);
+// }
+// foo();
 
-const boo = function () {
-  console.log(this);
+// const boo = function () {
+//   console.log(this);
+// };
+// boo();
+
+// const arrow = () => {
+//   console.log(this);
+// };
+
+// arrow();
+
+//call
+//apply
+//bind
+
+const objA = {
+  age: 22,
+  myAge() {
+    console.log(this);
+  },
 };
-boo();
 
-const arrow = () => {
-  console.log(this);
-};
-
-arrow();
+objA.myAge();
