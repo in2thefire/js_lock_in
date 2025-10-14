493745f2d57890ebb22ae4a97f0709c226f1acd2
Bohdan Kyrylko <in2thefire.code@gmail.com>
2025-10-14 12:01:38 +0200

    add html from js

--- module_6/script_6_1.js
+++ module_6/script_6_1.js
@@ -1,8 +1,22 @@
-const title = document.querySelector(".js_title");
-console.dir(title.textContent);
-title.textContent = "Hello Js";
-console.dir(title.classList);
+// const title = document.querySelector(".js_title");
+// console.dir(title.textContent);
+// title.textContent = "Hello Js";
+// console.dir(title.classList);
 
-title.classList.add("title-color");
-title.classList.remove("title-color");
-title.classList.toggle("title-color");
+// title.classList.add("title-color");
+// title.classList.remove("title-color");
+// title.classList.toggle("title-color");
+
+// console.log(title.getAttribute("data-id"));
+
+// // const li = document.createElement("li");
+// // li.textContent = list.children.length + 1;
+// // list.append(li);
+// // console.log(li);
+// const list = document.querySelector(".js-list");
+// console.dir(list.children);
+
+// const li = `<li class = "title-color" data-id ="1234">${
+//   list.children.length + 1
+// }<div><h2>Hello</h2></div></li>`;
+// list.insertAdjacentHTML("beforeend", li);
