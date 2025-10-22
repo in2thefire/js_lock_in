6bc71eb9bb03984e497f29fd7cfb75db6501efe5
Bohdan Kyrylko <in2thefire.code@gmail.com>
2025-10-22 16:15:35 +0200

    submit

--- module_6/script_6_2.js
+++ module_6/script_6_2.js
@@ -12,31 +12,38 @@
 //   console.log("Clock");
 // }
 
-const title = document.querySelector(".js-title");
-const title1 = document.querySelector(".js-title1");
-const title2 = document.querySelector(".js-title2");
-title.addEventListener("click", onClick);
-title1.addEventListener("click", onClick);
-title2.addEventListener("click", onClick);
+// const title = document.querySelector(".js-title");
+// const title1 = document.querySelector(".js-title1");
+// const title2 = document.querySelector(".js-title2");
+// title.addEventListener("click", onClick);
+// title1.addEventListener("click", onClick);
+// title2.addEventListener("click", onClick);
 
-const maxLength = 13;
-const totalLength = maxLength + 3;
+// const maxLength = 13;
+// const totalLength = maxLength + 3;
 
-function onClick(evt) {
-  const title = evt.currentTarget;
-  const str = title.textContent.slice(0, maxLength);
+// function onClick(evt) {
+//   const title = evt.currentTarget;
+//   const str = title.textContent.slice(0, maxLength);
 
-  if (title.textContent.length > totalLength) {
-    const remainder = title.textContent.slice(maxLength);
-    title.setAttribute("data-title", remainder);
-    title.textContent = str + "...";
-  } else {
-    const remainder = title.dataset["title"];
-    console.log(title.dataset["title"]);
-    console.log(title.dataset.title);
-    if (remainder) {
-      title.textContent = str + remainder;
-    }
-    // getAttribute
-  }
+//   if (title.textContent.length > totalLength) {
+//     const remainder = title.textContent.slice(maxLength);
+//     title.setAttribute("data-title", remainder);
+//     title.textContent = str + "...";
+//   } else {
+//     const remainder = title.dataset["title"];
+//     console.log(title.dataset["title"]);
+//     console.log(title.dataset.title);
+//     if (remainder) {
+//       title.textContent = str + remainder;
+//     }
+//     // getAttribute
+//   }
+// }
+
+const formEl = document.querySelector(".js-form");
+formEl.addEventListener("submit", onSubmit);
+
+function onSubmit(evt) {
+  evt.preventDefault();
 }
